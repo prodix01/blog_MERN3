f8245f881a60a8d97a47469a62899c165849ab64
kim <s158614@nishitech.ac.jp>
2019-11-26 16:45:03 +0900

    User Register

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,66 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+
+
+const userModel = require("../models/users");
 
 //회원가입
 // @route   POST    http://localhost:1234/users/register
 // @desc    register user
 // @access  public
 router.post("/register", (req, res) => {
+    userModel
+        .findOne({email : req.body.email})
+        .then(user => {
+            if (user) {
+                res.status(400).json({
+                    msg : "이미 계정이 존재합니다."
+                });
+            }
 
+            const avatar = gravatar.url(req.body.email, {
+                s : 200,    //size
+                r : "pg",   //rating
+                d : "mm"    //default
+            });
+
+            const newUser = new userModel({
+                name : req.body.userName,
+                email : req.body.email,
+                avatar : avatar,
+                password : req.body.password,
+            });
+
+            //패스워드 암호화
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt
+                    .hash(newUser.password, salt, (err, hash) => {
+                        if (err) throw err;
+                        newUser.password = hash;
+
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.status(200).json({
+                                    msg : "회원가입성공",
+                                    registerInfo : user
+                                });
+                            })
+                            .catch(err => {
+                                res.status(500).json({
+                                    error : err.message
+                                });
+                            });
+                    });
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error : err.message
+            });
+        });
 });
 
 
