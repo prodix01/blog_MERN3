fa90ac626e657cf5c2b7757502bd06468a7cdad8
kim <s158614@nishitech.ac.jp>
2019-12-23 13:09:08 +0900

    Add navbar and Footbar

---
+++ client/src/componets/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+
+class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+                <div className="container">
+                    <a className="navbar-brand" href="landing.html">DevConnect</a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#mobile-nav"
+                    >
+                        <span className="navbar-toggler-icon"/>
+                    </button>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
